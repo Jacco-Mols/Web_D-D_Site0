ddb054346d84eab7c06989c65d0d74a24b091726
Jacco12 <molsjacco@gmail.com>
2026-02-06 13:02:55 +0100

    changed book details and added styling

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 // pop up of books
 const popUp = document.querySelector('.pop-up')
-const bookHobby = document.querySelector('.bookHobby')
-
 const hidden = document.querySelector('.hidden')
 
+const bookButton = document.querySelector('.bookHobby')
 
-bookHobby.addEventListener('click', function() {
+
+
+const bookdetails = document.querySelector('.bookdetails')
+const pageTurnLeft = document.querySelector('.pageturn-left')
+const pageTurnRight = document.querySelector('.pageturn-right')
+
+
+bookButton.addEventListener('click', function() {
     console.log("works")
     popUp.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
 })
 
-popUp.addEventListener('click', function(){
+bookdetails.addEventListener('click', function() {
     popUp.classList.toggle('hidden')
+    document.body.classList.toggle('no-scroll')
+})
+
+
+pageTurnRight.addEventListener('click]', function() {
+    
 })
